661ad4fd858dfb28989f79ed97981742c1b25583
Alexander Rickett <alexander.rickett@king.com>
2017-02-03 21:42:29 +0100

    Added elasticsearch and logstash 1

--- src/main/script.sh
+++ src/main/script.sh
@@ -5,7 +5,7 @@ echo Creating logstash config file...
 
 echo -n "input { twitter { consumer_key => \"e9QWQDPxITiA8Ci2V2wQ3qSoQ\" consumer_secret =>\"yjmrNsPz3PHX2owLJ1xJ7UXxHDEkdhXiKggWXBoKJMJOelI5TY\" oauth_token => \"283137973-8igpqe6RQgNPPpdAs6qoE5fLO9vNkUtDccg0Q59P\" oauth_token_secret => \"3tc5eKuIoq0C5jprUCKnHT4W8nx5aqBGCBhQ6Q7RrlgAM\" keywords => [" > ./logstash_twitter.conf
 
-awk -F ";" '{print $1}' ../../resources/places.csv > ../../resources/file.csv
+awk -F ";" '{print $1}' ../../resources/places_test.csv > ../../resources/file.csv
 
 
 index=0
@@ -29,7 +29,7 @@ echo -n "] full_tweet => true } } filter { mutate { remove_field => [ \"extended
 echo Running ELK stack...
 (./kibana-5.1.1-darwin-x86_64/bin/kibana) &
 sleep 10
-(elasticsearch) &
+(./elasticsearch-5.2.0/bin/elasticsearch) &
 sleep 10
-(logstash -f logstash_twitter.conf) &
+(./logstash-5.2.0/bin/logstash -f logstash_twitter.conf) &
 wait
