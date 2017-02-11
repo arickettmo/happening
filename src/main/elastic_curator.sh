387c00ec6a5891ee2a20b68c6a4c1a666ea2992e
Alexander Rickett <alexander.rickett@king.com>
2017-02-11 18:59:45 +0100

    Fixed a bug in the curator script for cron

--- src/main/elastic_curator.sh
+++ src/main/elastic_curator.sh
@@ -1,1 +1,4 @@
-curator --config /home/ubuntu/happening/src/main/CONFIGURATION_FILE.yml /home/ubuntu/happening/src/main/ACTION_FILE.yml 
+#!/bin/bash
+
+export LC_ALL=en_US.utf8
+curator --config /home/ubuntu/happening/src/main/CONFIGURATION_FILE.yml --dry-run /home/ubuntu/happening/src/main/ACTION_FILE.yml 
