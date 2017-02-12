2a54e6e315120fa920f7d874273f159bebe3053f
Ubuntu <ubuntu@ip-172-31-17-236.eu-central-1.compute.internal>
2017-02-12 15:30:00 +0000

    UPDATE Logstash config file creating indices with hourly format. Action file of curator changed to delete indices older than one hour

--- src/main/generate_logstash_conf.sh
+++ src/main/generate_logstash_conf.sh
@@ -5,7 +5,7 @@ echo Creating logstash config file...
 
 echo -n "input { twitter { consumer_key => \"BYvnonTRKhTSMYEjZ1hqvbh4c\" consumer_secret =>\"n4XZz4wpcJWRYbLbhm1XlxvSxCzn4v8Rfg51omRblYX7qbab8I\" oauth_token => \"283137973-L97BLGeunsf8r8b5Xh8MDVeswFI0B93shjUJZ3OW\" oauth_token_secret => \"RL2iejRKSODZ7Ts6NwOLFpPjNj9vSqnNbrZZ8MTB4lt2G\" keywords => [" > /etc/logstash/conf.d/logstash_twitter.conf
 
-awk -F ";" '{print $1}' ../../resources/places_test.csv > ../../resources/file.csv
+awk -F ";" '{print $1}' ../../resources/places.csv > ../../resources/file.csv
 
 
 index=0
@@ -22,7 +22,7 @@ cat ../../resources/file.csv | while read line; do
 done
 
 
-echo -n "] full_tweet => true } } filter { mutate { remove_field => [ \"extended_entities\", \"retweeted_status\", \"entities\", \"user\" ] }} output { elasticsearch { hosts => \"http://localhost:9200\" document_type => \"tweet\" }}" >> /etc/logstash/conf.d/logstash_twitter.conf
+echo -n "] full_tweet => true } } filter { mutate { remove_field => [ \"extended_entities\", \"retweeted_status\", \"entities\", \"user\" ] }} output { elasticsearch { hosts => \"http://localhost:9200\" index => \"logstash-%{+YYYY.MM.dd.HH}\" document_type => \"tweet\" }}" >> /etc/logstash/conf.d/logstash_twitter.conf
 
 
 # Restart logstash
