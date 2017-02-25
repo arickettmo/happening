a4cf7f17924f8aa935e6e660e2aec91ffb1aa850
Ubuntu <ubuntu@ip-172-31-17-236.eu-central-1.compute.internal>
2017-02-25 12:33:31 +0000

    Trending places script now returning all the scores, means and statistics data

--- src/main/trending_places.py
+++ src/main/trending_places.py
@@ -5,6 +5,7 @@ from pprint import pprint
 import datetime
 from datetime import date, timedelta
 from time import strftime
+import statistics
 from elasticsearch import Elasticsearch
 import csv
 
@@ -15,7 +16,7 @@ if __name__ == "__main__":
     code=""
     places=[]
     rownum=0
-    with open('/Users/alexander.rickett/Desktop/happening/resources/places.csv', 'rU') as csvfile:
+    with open('/home/ubuntu/happening/resources/places.csv', 'rU') as csvfile:
         places_file = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in places_file:
             if rownum!=0:
@@ -34,56 +35,69 @@ if __name__ == "__main__":
     
     now = datetime.datetime.now()
     timestamp = datetime.datetime(now.year, now.month, now.day, now.hour).isoformat()
-    
-    # This is to get the difference of days between the first point of data
-    today = datetime.date.today()
-    someday = datetime.date(2017, 2, 22)
-    diff = abs(someday - today).days
-    
+       
+
     for place in places:
         for index in range(-1, 10):
             if index==-1:
                 # req_head
                 search_arr.append({ })
                 # req_body
-                search_arr.append({"query": {"bool": {"must": [{"range": {"@timestamp":{"eq": timestamp}}},{"match": {"text": {"query": place["country"],"operator": "and"}}}]}}})
+                search_arr.append({"query":{"bool":{"must":[{"term":{"country.country.keyword":place["country"]}},{"term":{"timestamp":timestamp}}]}}})
             else:
                 # req_head
                 search_arr.append({ })
                 # req_body
-                days = diff + index
-                search_arr.append({"query": {"bool": {"must": [{"range": {"@timestamp":{"eq": timestamp+"-"+str(days)+"d"}}},{"match": {"text": {"query": place["country"],"operator": "and"}}}]}}})
+                # Temporary as we have data fro Feb 13th to Feb 22nd
+                day = 22 - index
+                timestamp = datetime.datetime(2017, 2, day, now.hour).isoformat()
+                search_arr.append({"query":{"bool":{"must":[{"term":{"country.country.keyword":place["country"]}},{"term":{"timestamp":timestamp}}]}}})
 
     request = ''
     for each in search_arr:
         request += '%s \n' %json.dumps(each)
-        
-    print request
-    
-#    response = es.msearch(body=request)
-#    
-#    
-#    places_volume={}
-#    data={ "country": [], "code": [], "volume_a": [], "volume_b": [], "diff": [] }
+   
+
+
+    response = es.msearch(body=request)
+   
+# Returning data for plotly:
+#
+#    data={ "country": [], "code": [], "median": [], "stdev": [], "score": [] }
 #    pointer=0
 #    for index,place in enumerate(places):
-#        for index_b in range(0, 2):
-#            if index_b==0:
-#                volume_a=response["responses"][pointer+index_b]["hits"]["total"]
-#                if volume_a==0:
-#                    volume_a=1
-#            else:
-#                volume_b=response["responses"][pointer+index_b]["hits"]["total"]
-#                diff=((volume_b-volume_a)/volume_a)*100
-#        pointer+=2
+#	values = []
+#        for index_b in range(0, 11):
+#            values.append(response["responses"][pointer+index_b]["hits"]["hits"][0]["_source"]["volume"])
 #        data["country"].append(place["country"])
 #        data["code"].append(place["code"])
-#        data["volume_a"].append(volume_a)
-#        data["volume_b"].append(volume_b)
-#        data["diff"].append(diff)
+#        data["median"].append(statistics.mean(values))
+#        data["stdev"].append(statistics.stdev(values))
+#        if statistics.stdev(values)==0:
+#            score = 0
+#        else:
+#	    score = (response["responses"][pointer]["hits"]["hits"][0]["_source"]["volume"]-statistics.mean(values))/statistics.stdev(values)
+#        data["score"].append(score)
+#        pointer+=11
+ 
+
+    data=[]
+    pointer=0
+    for index,place in enumerate(places):
+        values = []
+        for index_b in range(0, 11):
+            values.append(response["responses"][pointer+index_b]["hits"]["hits"][0]["_source"]["volume"])
+        if statistics.stdev(values)==0:
+            score = 0
+        else:
+            score = (response["responses"][pointer]["hits"]["hits"][0]["_source"]["volume"]-statistics.mean(values))/statistics.stdev(values)
+	data.append({"country": place["country"], "code": place["code"], "mean": statistics.mean(values), "stdev": statistics.stdev(values), "score": score})
+        pointer+=11
+
+    pprint(data)   
 #    print "Content-type: text/html\n\n";
 #    print json.dumps(data)   
-#    
+    
     
     
     
